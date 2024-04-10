13e959b6b1c62dce7ad02d85693888c21b5ef412
DAI RE <112737993+dai-re@users.noreply.github.com>
2024-04-10 12:31:15 +0700

    clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/**/*{html, js}"],
+  content: [
+    "./layouts/**/**/*{html, js}",
+    "./assets/css/input.css"
+  ],
   theme: {
     extend: {},
   },
